d853eb8911ab03c93a04d9e8f18e85645397af81
Siddarth5199 <42136857+Siddarth5199@users.noreply.github.com>
2020-05-10 19:27:32 +0530

    added all operations

--- src/main/java/calc/Calculator_devops.java
+++ src/main/java/calc/Calculator_devops.java
@@ -8,6 +8,36 @@ public class Calculator_devops
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+        double number_first = 16.0;
+        double number_second = 4.0;
+
+        char operator = '*';
+
+        double output;
+
+        switch(operator)
+        {
+            case '+':
+                output = number_first + number_second;
+                break;
+
+            case '-':
+                output = number_first - number_second;
+                break;
+
+            case '*':
+                output = number_first * number_second;
+                break;
+
+            case '/':
+                output = number_first / number_second;
+                break;
+
+            default:
+                System.out.printf("This operator is not supported");
+                return;
+        }
+
+        System.out.printf("output of the operation is %f ", output);
+    }    }
 }
